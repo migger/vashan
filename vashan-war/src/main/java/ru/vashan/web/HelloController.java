66f99730ddf10241ed7184062835c9e31bc33364
migger <migger@yandex.ru>
2014-02-14 19:15:21 +0400

    Added Spring MVC

--- vashan-war/src/main/java/ru/vashan/web/HelloController.java
+++ vashan-war/src/main/java/ru/vashan/web/HelloController.java
@@ -3,11 +3,13 @@ package ru.vashan.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/index.html")
 public class HelloController {
-    @RequestMapping(method = RequestMethod.GET,  produces = "text/plain")
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
     public String sayHello() {
         return "Hello, vashan";
     }
